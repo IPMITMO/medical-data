f012a67fad80278a19b682266d66fc4faa4b8870
Tatarsky Vladimir <tatarsky.v@mail.ru>
2016-10-18 19:44:50 +0300

    This closes #2

---
+++ src/clean.rb
@@ -0,0 +1,28 @@
+area, street, id_exitus, id_prvs, main = {}, {}, {}, {}, {}
+
+# area
+area_raw = File.open('raw/area.csv').readlines
+area_raw.map{ |item| area[item.split(";")[1]] = item.split(";")[2].delete!("\n") }
+area.delete "IDENT"
+
+# street
+street_raw = File.open('raw/street.csv').readlines
+street_raw.map{ |item| street[item.split(";")[1]] = item.split(";")[2].delete!("\n") }
+street.delete "IDENT"
+
+# id_exitus
+id_exitus_raw = File.open('raw/id_exitus.csv').readlines
+id_exitus_raw.map{ |item| id_exitus[item.split(";")[1]] = item.split(";")[2].delete!("\n") }
+id_exitus.delete "ABBREVIATION"
+
+# id_exitus
+id_prvs_raw = File.open('raw/id_prvs.csv').readlines
+id_prvs_raw.map{ |item| id_prvs[item.split(";")[1]] = item.split(";")[2].delete!("\n") }
+id_prvs.delete "CODE"
+
+main_raw = File.open('raw/main.csv').readlines
+main_raw = main_raw[1..10].map{|item| item.split(";")}
+main = main_raw.each do |item|
+  printf "%-20s %-2s\t%-20s\t%-10s\t%-20s\n", item[1], item[2], street[item[3]], item[7], id_prvs[item[9]], id_exitus[item[10]]
+  # puts "#{item[1]}\t#{item[2]}\t#{street[item[3]]}   \t\t#{item[7]} \t#{id_prvs[item[9]]}   \t#{id_exitus[item[10]]} "
+end
--- src/mapper.py
+++ src/mapper.py
@@ -1,8 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-def do(arr):
-    m = {}
-    for elem in range(1, len(arr)):
-        m[arr[elem]['abbr']] = arr[elem]['name']
-    return m
